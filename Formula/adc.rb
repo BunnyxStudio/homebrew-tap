a7392ebd34674f6fab5a61bad7851e1d20380c32
Bunny <118424673+BunnyxStudio@users.noreply.github.com>
2026-04-07 21:04:54 +0800

    Release adc 0.1.0

--- Formula/adc.rb
+++ Formula/adc.rb
@@ -1,9 +1,9 @@
 class Adc < Formula
   desc "Read-only CLI for querying App Store Connect reporting data"
   homepage "https://github.com/BunnyxStudio/app-connect-data-cli"
-  url "https://github.com/BunnyxStudio/app-connect-data-cli/archive/b5fbde11ba1757c1ba26cce03e3262343867b5a6.tar.gz"
-  version "0.1.0-b5fbde1"
-  sha256 "46d2041d03821c36b683c0a0497e465c73bbbae2c5b0c20713c32c07a9064d69"
+  url "https://github.com/BunnyxStudio/app-connect-data-cli/archive/refs/tags/v0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "e8e71685ac174895661fa963c0af40ebccbb412bb2c93012c881331991968e36"
   license "Apache-2.0"
 
   resource "swift-argument-parser" do
